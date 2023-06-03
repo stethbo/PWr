27e544a06e959badd69279319012420584a7dedf
stethbo <sztefan.borek@gmail.com>
2023-06-03 09:07:26 +0200

    adds clean up and dir names for models

--- zpi/train-cl/train_cl_checkpoints.py
+++ zpi/train-cl/train_cl_checkpoints.py
@@ -35,17 +35,18 @@ EPOCHS = CONFIG.get('epochs')
 BATCH_SIZE = CONFIG.get('batch_size')
 WARMAP_STEPS = CONFIG.get('warmup_steps')
 USE_AMP = CONFIG.get('use_amp')
-CHECKPOINTS_DIR = CONFIG.get('checkpoint_dir')
 CHECKPOINT_SAVE_STEPS = CONFIG.get('checkpoint_save_steps')
 
 logger.info('All configuration variables loaded successfully')
 
 random.seed(SEED)
 
+def clean_up_data(data: pd.DataFrame) -> pd.DataFrame:
+    data.dropna(inplace=True)
+    return data
 
 
-def train_model(model_name):
-    logger.info(f'Checkpoints directory exists: {os.path.exists(CHECKPOINTS_DIR)}')
+def train_model(model_name: str, dir_name: str):
     try:
         sentences = pd.read_csv(DATA_FILE)
     except Exception as e:
@@ -54,15 +55,16 @@ def train_model(model_name):
 
     logger.info(f'Data loaded successfully')
 
+    sentences = clean_up_data(sentences)
+    logger.info(f'Data cleaned up successfully')
+
     train_data = [InputExample(texts=[s, s]) for s in sentences['content']]
     train_dataloader = DataLoader(
         train_data, 
-        batch_size=BATCH_SIZE, 
+        batch_size=BATCH_SIZE,
         shuffle=True)
 
     logger.info(f'Creating torch dataset completed')
-
-
 
     word_embedding_model = models.Transformer(model_name, max_seq_length=MAX_SEQ_LENGTH)
     logger.info('Word embedding model created successfully')
@@ -70,22 +72,20 @@ def train_model(model_name):
     pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension())
     logger.info('Pooling model created successfully')
 
-
-
     model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
     logger.info('Sentence Transformer model for CL created successfully')
     train_loss = losses.MultipleNegativesRankingLoss(model)
     logger.info(f'Train loss set properly')
 
-    logger.info(f'calling model.fit() on {model_name}')
-
+    checkpoints_dir = os.path.join(os.getcwd(), dir_name)
+    logger.info(f'Checkpoints directory: {checkpoints_dir}')
     model.fit(
         train_objectives=[(train_dataloader, train_loss)],
         epochs=EPOCHS,
         show_progress_bar=True,
         warmup_steps=WARMAP_STEPS,
         use_amp=USE_AMP,
-        checkpoint_path=CHECKPOINTS_DIR,
+        checkpoint_path=checkpoints_dir,
         checkpoint_save_steps=CHECKPOINT_SAVE_STEPS,
     )
 
@@ -94,13 +94,13 @@ def train_model(model_name):
     logger.info(f'Fine tuned model {OUTPUT_MODEL_NAME + model_name} saved successfully')
 
 if __name__ == '__main__':
-    model_names = [
-        'sentence-transformers/paraphrase-multilingual-mpnet-base-v2',
-        'sentence-transformers/distiluse-base-multilingual-cased-v1',
-    ]
+    model_names = {
+        'sentence-transformers/paraphrase-multilingual-mpnet-base-v2': 'models/paraphrase-multilingual-mpnet-base-v2',
+        'sentence-transformers/distiluse-base-multilingual-cased-v1': 'models/distiluse-base-multilingual-cased-v1',
+    }
 
-    for model in model_names:
+    for model, dir_name in model_names.items():
         try:
-            train_model(model)
+            train_model(model, dir_name)
         except Exception as e:
             logger.error(f'Error while training model {model}: {e}')
