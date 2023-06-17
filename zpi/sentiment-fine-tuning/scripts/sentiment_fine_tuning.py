331ae1ebdd92eed29f09e636e0af945e209ee361
stethbo <sztefan.borek@gmail.com>
2023-06-17 14:20:48 +0200

    improves model finetuning script

--- zpi/sentiment-fine-tuning/scripts/sentiment_fine_tuning.py
+++ zpi/sentiment-fine-tuning/scripts/sentiment_fine_tuning.py
@@ -13,14 +13,22 @@ MODEL_NAME = 'xlm-roberta-base'
 DATA_PATH = '/home/stefan/PWr/uni/PWr/zpi/sentiment-fine-tuning/data/data_labeled_remapped.xlsx'
 TEXT_COLUMN = 'content'
 LABEL_COLUMN = 'label'
+NUM_LABELS = 3
 SPLIT = 0.8
+
+# Training arguments
+EPOCHS = 24
+BATCH_SIZE = 64
+WARMPUP_STEPS = 500
+WEIGHT_DECAY = 0.01
+OUTPUT_DIR = './sentiment_fine_tuned_models'
 
 
 class MyDataset(torch.utils.data.Dataset):
 
-    def __init__(self, reviews, sentiments, tokenizer):
-        self.reviews    = reviews
-        self.sentiments = sentiments
+    def __init__(self, contents, labels, tokenizer):
+        self.contents    = contents
+        self.labels = labels
         self.tokenizer  = tokenizer
         self.max_len    = tokenizer.model_max_length
   
@@ -28,11 +36,11 @@ class MyDataset(torch.utils.data.Dataset):
         return len(self.reviews)
   
     def __getitem__(self, index):
-        review = str(self.reviews[index])
-        sentiments = self.sentiments[index]
+        content = str(self.reviews[index])
+        label = self.labels[index]
 
         encoded_review = self.tokenizer.encode_plus(
-            review,
+            content,
             add_special_tokens    = True,
             max_length            = self.max_len,
             return_token_type_ids = False,
@@ -45,7 +53,7 @@ class MyDataset(torch.utils.data.Dataset):
         return {
             'input_ids': encoded_review['input_ids'][0],
             'attention_mask': encoded_review['attention_mask'][0],
-            'labels': torch.tensor(sentiments, dtype=torch.long)
+            'labels': torch.eye(NUM_LABELS)[label]
         }
 
 
@@ -62,7 +70,7 @@ def convert_sentiment(sentiment):
     return 1 if sentiment == 'Positive' else 0
 
 
-def cleanup_text(text):
+def cleanup_text(text: str) -> str:
     # Lowercase the text
     text = str(text).lower()
     
@@ -86,7 +94,7 @@ def cleanup_text(text):
 def compute_metrics(pred):
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='weighted')
     acc = accuracy_score(labels, preds)
     return {
         'accuracy': acc,
@@ -96,12 +104,12 @@ def compute_metrics(pred):
     }
 
 
-
 if __name__ == "__main__":
     df = read_data(DATA_PATH)
     dataset = pd.DataFrame()
     dataset['text'] = df[TEXT_COLUMN].apply(cleanup_text)
     dataset['label'] = df[TEXT_COLUMN].apply(convert_sentiment)
+    
     dataset.dropna(inplace=True)
 
     train_set = dataset[:int(len(dataset) * SPLIT)]
@@ -127,13 +135,13 @@ if __name__ == "__main__":
     # Create DataLoader for train/validation sets.
     train_set_dataloader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_size  = 16,
+        batch_size  = BATCH_SIZE,
         num_workers = 4
     )
 
     valid_set_dataloader = torch.utils.data.DataLoader(
         test_dataset,
-        batch_size  = 16,
+        batch_size  = BATCH_SIZE,
         num_workers = 4
     )
 
@@ -142,9 +150,11 @@ if __name__ == "__main__":
     valid_data = next(iter(valid_set_dataloader))
 
     # Print the output sizes.
-    logger.info(f'Sizes of taining data batches: {train_data["input_ids"].size()}, {valid_data["input_ids"].size()}')
+    logger.info(f'Sizes of taining data batches: {train_data["input_ids"].size()},\
+                 {valid_data["input_ids"].size()}')
 
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=2)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME,
+                                                               num_labels=NUM_LABELS)
 
     for i, (name, param) in enumerate(model.named_parameters()):
         if name.startswith('classifier'):
@@ -156,12 +166,12 @@ if __name__ == "__main__":
     logger.info(f'Model set up successfully: {model.name_or_path}')
 
     training_args = TrainingArguments(
-        output_dir                  = "./sentiment-analysis",
-        num_train_epochs            = 24,
+        output_dir                  = OUTPUT_DIR,
+        num_train_epochs            = EPOCHS,
         per_device_train_batch_size = 128,
         per_device_eval_batch_size  = 64,
-        warmup_steps                = 500,
-        weight_decay                = 0.01,
+        warmup_steps                = WARMPUP_STEPS,
+        weight_decay                = WEIGHT_DECAY,
         save_strategy               = "epoch",
         evaluation_strategy         = "steps"
     )
